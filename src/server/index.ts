7ada18fc74fb22eaf99e18ec09a3e9557aafad34
maxijayme <maxijayme@gmail.com>
2023-08-16 12:04:02 +0200

    rendering dynamic h1 from server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,10 @@
 import express, {Express, Request, Response} from 'express';
 import { config } from '../server/config'
+import { template } from './render/template';
 const app: Express = express();
 
 app.get('*',(req: Request, res: Response)=>{
-    res.send(`<h1>Estas en la ruta: ${req.url} </h1>`)
+    res.send(template(`<h1>Render h1 from server</h1>`))
 })
 
 app.listen(config.PORT, ()=> console.log(`Listing port ${config.PORT}`))
