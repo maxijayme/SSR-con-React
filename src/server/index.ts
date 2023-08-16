b70bc605bada5f72e24d4647ba860cc2262b876d
maxijayme <maxijayme@gmail.com>
2023-08-16 11:53:05 +0200

    rendering h1 from server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,9 @@
-console.log("Hola mundo desde el server")
+import express, {Express, Request, Response} from 'express';
+import { config } from '../server/config'
+const app: Express = express();
+
+app.get('*',(req: Request, res: Response)=>{
+    res.send(`<h1>Estas en la ruta: ${req.url} </h1>`)
+})
+
+app.listen(config.PORT, ()=> console.log(`Listing port ${config.PORT}`))
